fa8e0831560d9e181f8fcfbb2ad9f012477ecc7e
Abdurrahman Çevik <abdurrahmancevik2005@gmail.com>
2025-02-06 21:57:53 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,40 @@
+// 4 BOYUT TESTİ
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // 4. BOYUT
+    int arr[4][4][4][4];
+
+    int sayi = 0;
+    string test1 = "0" + sayi;
+
+    for(int i=0; i<4; i++){
+        for(int j=0; j<4; j++){
+            for(int k=0; k<4; k++){
+                for(int l=0; l<4; l++){
+                static int sayi=100;
+                arr[i][j][k][l] = sayi;
+                sayi++;
+                }
+            }
+        }
+    }
+
+
+    for(int i=0; i<4; i++){
+        for(int j=0; j<4; j++){
+            for(int k=0; k<4; k++){
+                for(int l=0; l<4; l++){
+                cout << arr[i][j][k][l] << " ";
+                }
+                cout << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
